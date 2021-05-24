a117d2ec8ed5d569fa43667197e62273b1171020
SaiidZafari <saiid.zafari@bahnhof.se>
2021-05-24 16:50:26 +0200

    Create Initial Commit

--- Studentify.Api/Models/AppDbContext.cs
+++ Studentify.Api/Models/AppDbContext.cs
@@ -5,7 +5,18 @@ using System.Threading.Tasks;
 
 namespace Studentify.Api.Models
 {
-    public class AppDbContext
-    {
-    }
+    //public class AppDbContext : DbContext
+    //{
+    //    public AppDbContext(DbContextOptions<AppDbContext> options)
+    //    : base(options)
+    //    {
+    //    }
+
+    //    public DbSet<Teacher> Teachers { get; set; }
+
+    //    public DbSet<Course> Subjects { get; set; }
+
+    //    public DbSet<Student> Students { get; set; }
+
+    //}
 }
