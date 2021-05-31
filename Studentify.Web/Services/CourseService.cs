0d46f092363aca88ba3ed36ccf0ac89b00fe3658
Adrian-FL <ratzione@hotmail.com>
2021-05-31 09:28:34 +0200

    add footer & listed courses

---
+++ Studentify.Web/Services/CourseService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using Studentify.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Studentify.Web.Services
+{
+    public class CourseService : ICourseService
+    {
+        private readonly HttpClient httpClient;
+
+        public CourseService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+        public async Task<IEnumerable<Course>> GetCourses()
+        {
+            return await httpClient.GetJsonAsync<Course[]>("api/courses");
+        }
+    }
+}
