1ce37ba771ebaccd17002c5372936adb8487e550
SaiidZafari <saiid.zafari@bahnhof.se>
2021-06-01 18:06:49 +0200

    Add Delete function

--- Studentify.Api/Controllers/TeachersController.cs
+++ Studentify.Api/Controllers/TeachersController.cs
@@ -136,7 +136,7 @@ namespace Studentify.Api.Controllers
             try
             {
                 var result = await teacherRepository.Search(name);
-                if (result.Any())
+                if (result != null)
                 {
                     return Ok(result);
                 }
