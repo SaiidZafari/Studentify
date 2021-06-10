3cf8142f8773bfcbe2cf2b97bf369677a788cdf7
Jonathan Odeldahl <airaktai@gmail.com>
2021-06-10 13:19:09 +0200

    More changes

--- Studentify.Api/MockData.sql
+++ Studentify.Api/MockData.sql
@@ -3,9 +3,10 @@ GO
 
 INSERT INTO [dbo].[Teachers] ([TeacherName], [ImageUrl])
 VALUES
-('John Doe', 'https://via.placeholder.com/180x200.png?text=John+Doe'),
-('Jane Doe', NULL),
-('Lars Erik Robert Tublen', NULL)
+('John Doe', '/images/Teacher.png'),
+('Jane Doe','/images/Teacher1.png'),
+('Lars Erik Robert Tublen', '/images/Teacher2.png'),
+('Rutger Hansson', NULL)
 GO
 
 INSERT INTO [dbo].[Subjects] ([SubjectName], [Description], [ImageUrl])
@@ -16,20 +17,20 @@ GO
 
 INSERT INTO [dbo].[Courses] ([SubjectId], [TeacherId], [CourseName], [Description], [ImageUrl])
 VALUES
-(2, 1, 'Basic Math', '1+1=2 and so on.', 'https://via.placeholder.com/320x320.png?text=Math+101'),
-(2, 2, 'Advanced Math', '1*1=1 and other stuff', 'https://via.placeholder.com/320x320.png?text=Complex+Math'),
-(2, 3, 'WebAPI', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://via.placeholder.com/320x320.png?text=WebAPI'),
-(2, 3, 'Asp.NET Core', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://via.placeholder.com/320x320.png?text=Asp.NET+Core'),
-(2, 3, 'Javascript', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://via.placeholder.com/320x320.png?text=Javascript')
+(2, 1, 'Basic Math', 'Practical mathematics has been a human activity from as far back as written records exist. The research required to solve mathematical problems can take years or even centuries of sustained inquiry.', '/images/basic_math.jpg'),
+(2, 2, 'Advanced Math', 'Mathematics always played a special role in scientific thought, serving since ancient times as a model of truth and rigor for rational inquiry, and giving tools or even a foundation for other sciences (especially physics).', '/images/advanced_math.jpg'),
+(2, 3, 'WebAPI', 'A Web API is an application programming interface for either a web server or a web browser.', '/images/webapi.jpg'),
+(2, 3, 'Asp.NET Core', 'ASP.NET Core is a free and open-source web framework and successor to ASP.NET, developed by Microsoft. It is a modular framework that runs on both the full .NET Framework, on Windows, and the cross-platform .NET Core.', '/images/asp_net_core.jpg'),
+(2, 3, 'Javascript', 'Alongside HTML and CSS, JavaScript is one of the core technologies of the World Wide Web. Over 97% of websites use it client-side for web page behaviour.', '/images/javascript.jpg')
 GO
 
 INSERT INTO [dbo].[Students] ([StudentName], [ImageUrl])
 VALUES
 ('James Doe', 'https://via.placeholder.com/180x200.png?text=James+Doe'),
-('Adrian', NULL),
-('Jonathan', NULL),
-('Mikael', NULL),
-('Saiid', NULL)
+('Adrian', '/images/StudentD1.png'),
+('Jonathan', '/images/StudentD-2.png'),
+('Mikael', '/images/StudentD-3.png'),
+('Saiid', '/images/StudentD-4.png')
 GO
 
 INSERT INTO [dbo].[Grades] ([StudentId], [CourseId], [StudentGrade])
